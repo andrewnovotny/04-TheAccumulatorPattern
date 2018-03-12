f62311d38dcb956dd8b8aed82fa42a722f054bb0
novotnaj <novotnaj@rose-hulman.edu>
2018-03-12 11:40:28 -0400

    m3 done

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Andrew Novotny.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -88,7 +88,15 @@ def draw_parallel_lines(n, point, length, window):
         -- Each rg.Line has the given length.
         -- Each rg.Line is 30 pixels below the previous rg.Line.
       Must  ** render **     but   ** NOT close **   the window.
+    """
+    for k in range(n):
+        end = rg.Point(point.x + length, point.y)
+        line = rg.Line(point, end)
+        line.attach_to(window)
+        point.y = point.y + 30
+    window.render()
 
+    """
     Type hints:
       :type n: int
       :type point: rg.Point
@@ -96,7 +104,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -143,7 +151,14 @@ def draw_lines(n, point, window):
     Side effects:
       See   draw_lines.pdf   in this project for pictures that
         may help you better understand the following specification:
-
+    """
+    end = rg.Point(point.x + 100, point.y - 100)
+    for k in range(n):
+        line = rg.Line(point, end)
+        line.attach_to(window)
+        window.render()
+        end.y = end.y + 200/(n - 1)
+    """
       Draws  n  rg.Lines on the given rg.RoseWindow, such that:
         -- The leftmost point of each of the rg.Lines
              is the given rg.Point.
@@ -161,7 +176,7 @@ def draw_lines(n, point, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
